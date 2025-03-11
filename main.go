462dcddc6e298936101690c3d1352680be44abdd
mombewa <ombewamaurice79@gmail.com>
2025-03-11 12:01:56 +0300

    feat: implement main function to store student records Added main function to accept student details (name, roll number, and branch) as command-line arguments and store them in an SQLite database. The program initializes the database, creates the students table if it doesn't exist, and inserts the provided data. Includes error handling for database operations and input validation.

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"concepts/database"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	name := os.Args[1]
+	rollnum := os.Args[2]
+	branch := os.Args[3]
+
+	rollno, err := strconv.Atoi(rollnum)
+	if err != nil {
+		log.Fatal("Error converting rollnum to integer: ", err)
+	}
+
+	//create database
+	db, err := database.CreateDb()
+	if err != nil {
+		log.Fatal("Error opening database: ", err)
+	}
+
+	//create students table
+	err = database.CreateTable(db)
+	if err != nil{
+		log.Fatal("Error creating table students: ", err)
+	}
+
+	err = database.StoreInDb(db, name, branch, rollno)
+	if err != nil {
+		log.Fatal("Error inserting data to table students: ", err)
+	}
+
+	// fetchFromDb(name,rollno,branch)
+}
